6ec36956d7dc44c52862b5c987af618e8f881fe0
creedx <guilhermejeffofc@gmail.com>
2024-06-12 10:34:50 -0300

    User and Admin Registration Controller

--- app/Http/Controllers/Admin/AdminRegistationController.php
+++ app/Http/Controllers/Admin/AdminRegistationController.php
@@ -4,8 +4,23 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class AdminRegistationController extends Controller
 {
-    //
+    public function create(){
+        return view('admin.create');
+    }
+
+    public function store(Request $request){
+        $input = $request->all();
+        User::create([
+            'name' => $input['name'],
+            'email' => $input['email'],
+            'password'=> bcrypt($input['password']),
+            'role' => 'admin',
+        ]);
+
+        return view('admin.thank');
+    }
 }
--- app/Http/Controllers/User/UserRegistationController.php
+++ app/Http/Controllers/User/UserRegistationController.php
@@ -4,8 +4,24 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserRegistationController extends Controller
 {
-    //
+    public function create(){
+
+        return view("user.create");
+    }
+
+    public function store(Request $request){
+        $input = $request->all();
+        $user = User::create([
+            "name"=> $input["name"],
+            "email"=> $input["email"],
+            "password"=> bcrypt($input["password"]),
+            'role' => 'user',
+        ]);
+
+        return view('user.thank');
+    }
 }
